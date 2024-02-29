9ca738ea80d6a1dbbf5876e9487d3bc1610c1e11
David A. Rios S <64142938+riosd252@users.noreply.github.com>
2024-02-29 10:32:36 +0100

    Create AddressesService.java

---
+++ src/main/java/com/team1/epicenergyservices/services/AddressesService.java
@@ -0,0 +1,23 @@
+package com.team1.epicenergyservices.services;
+
+import com.team1.epicenergyservices.entities.Address;
+import com.team1.epicenergyservices.payloads.AddressDTO;
+import com.team1.epicenergyservices.repositories.AddressesDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressesService {
+
+    @Autowired
+    private AddressesDAO addressesDAO;
+
+    @Autowired
+    private MunicipalitiesService municipalitiesService;
+
+    public Address save(AddressDTO addressDTO) {
+        Address newAddress = new Address(addressDTO.street(), addressDTO.civicNumber(), addressDTO.location(),
+                addressDTO.cap(), municipalitiesService.findByName(addressDTO.municipality()));
+        return addressesDAO.save(newAddress);
+    }
+}
